159b6fc5b395a06ed29b4d31a09cada1750fb6d2
Mohammed Murshid <murshidmp4@gmail.com>
2025-01-12 12:25:34 +0530

    Update greeting message in AppService to include timestamp

--- src/app.service.ts
+++ src/app.service.ts
@@ -6,6 +6,6 @@ export class AppService {
   constructor(private readonly logger: LoggerService) {}
   getHello(): string {
     this.logger.warn('Hello World! loggedd');
-    return 'Hello World!';
+    return 'Hello World! jan 12 12:00PM';
   }
 }
